0a647bf08b12967ddf3535812db7c380f07799cd
cheesecake0707 <65722432+cheesecake0707@users.noreply.github.com>
2020-06-12 23:48:10 -0400

    6.12.2020_draft1

--- dashboard_generator.py
+++ dashboard_generator.py
@@ -1,19 +1,46 @@
 # dashboard_generator.py
 
-print("-----------------------")
-print("MONTH: March 2018")
+#input
+import os
+import glob
+path = "/Users/Renee/Documents/GitHub/exec-dash-project/monthly-sales"
+extension = "csv"
+os.chdir(path)
+file_name = glob.glob("*.{}".format(extension))
+print(file_name)
 
-print("-----------------------")
-print("CRUNCHING THE DATA...")
+user_choice = input("Please choose from the above file list (without ''): ")
 
-print("-----------------------")
-print("TOTAL MONTHLY SALES: $12,000.71")
+import csv
 
-print("-----------------------")
-print("TOP SELLING PRODUCTS:")
-print("  1) Button-Down Shirt: $6,960.35")
-print("  2) Super Soft Hoodie: $1,875.00")
-print("  3) etc.")
+csv_file_path = user_choice
 
-print("-----------------------")
-print("VISUALIZING THE DATA...")
+with open(csv_file_path, "r") as csv_file:
+    reader = csv.DictReader(csv_file)
+    for row in reader:
+        print(row["date"], row["product"], row["unit price"], row["units sold"], row["sales price"])
+
+#validate
+
+if user_choice not in file_name:
+    print("Hey, didn't find a file at that location")
+    exit()
+
+
+#print("-----------------------")
+#print("MONTH: March 2018")
+
+#print("-----------------------")
+#print("CRUNCHING THE DATA...")
+
+#print("-----------------------")
+#print("TOTAL MONTHLY SALES: $12,000.71")
+
+#print("-----------------------")
+#print("TOP SELLING PRODUCTS:")
+#print("  1) Button-Down Shirt: $6,960.35")
+#print("  2) Super Soft Hoodie: $1,875.00")
+#print("  3) etc.")
+
+#print("-----------------------")
+#print("VISUALIZING THE DATA...")
